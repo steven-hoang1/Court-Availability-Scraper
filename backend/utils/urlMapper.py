e1299f0dee29e990328f595a618021e2fd8133f5
steven-hoang1 <stevenhoang8@gmail.com>
2025-07-07 09:45:05 +1000

    refactored urlMapper

--- backend/utils/urlMapper.py
+++ backend/utils/urlMapper.py
@@ -1,11 +1,11 @@
-from .constants import constants
+from . import constants
 
 class urlMapper:
     @staticmethod
     def Map(locationNumber):
-        if locationNumber == 2 or 3 or 4 or 5 or 6:
+        if locationNumber in [2, 3, 4, 5, 6]:
             return constants.CITY_COMMUNITY_TENNIS_URL
-        elif locationNumber == 55 or 72:
+        elif locationNumber in [55, 72]:
             return constants.PARKLANDS_URL
         elif locationNumber == 43:
             return constants.LANGHAM_HOTEL_URL
