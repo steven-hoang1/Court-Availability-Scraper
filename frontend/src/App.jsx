93d1ec294142ea0c0f00d924b27ecec6bac16cda
steven-hoang1 <stevenhoang8@gmail.com>
2025-07-08 21:26:50 +1000

    testing getting from backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import styles from './App.module.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <h1 className={styles.title}>Hello Tennis World 🎾</h1>
-  )
-}
+  useEffect(() => {
+      fetch('http://3.104.109.133/scrape/location=6')
+        .then((res) => res.json())
+        .then((json) => setData(json))
+        .catch((err) => console.error('API error:', err));
+    }, []);
 
-export default App
+    return (
+      <div>
+        <h1>Data from FastAPI backend:</h1>
+        <pre>{data ? JSON.stringify(data, null, 2) : 'Loading...'}</pre>
+      </div>
+    );
+  }
+
+export default App;
