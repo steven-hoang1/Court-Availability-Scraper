35d345c2b4593aaa9017c9d79ab8a3706d5dd462
steven-hoang1 <stevenhoang8@gmail.com>
2025-07-08 21:55:39 +1000

    removed repeated import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import styles from './App.module.css';
 import { useEffect, useState } from 'react';
 
 function App() {
