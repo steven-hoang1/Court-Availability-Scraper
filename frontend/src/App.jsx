e8b229e7cbd257bea6092e2ab924fa90b21a6d21
steven-hoang1 <stevenhoang8@gmail.com>
2025-07-08 23:11:44 +1000

    testing table format

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,39 @@ function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-      fetch('https://api-last-minute-tennis.com/scrape/location=6')
+      fetch('https://api-last-minute-tennis.com/scrape/location=55')
         .then((res) => res.json())
         .then((json) => setData(json))
         .catch((err) => console.error('API error:', err));
     }, []);
 
     return (
-      <div>
-        <h1>Data from FastAPI backend:</h1>
-        <pre>{data ? JSON.stringify(data, null, 2) : 'Loading...'}</pre>
-      </div>
-    );
+    <div>
+      <h1>Centennial Park</h1>
+      {data ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Courts Available</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((slot, idx) => (
+              <tr key={idx}>
+                <td>{slot.date}</td>
+                <td>{slot.time}</td>
+                <td>{slot.courts_available}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
   }
 
 export default App;
